155cf865f98a8eb3b48e50a835549e1d8b3e2c94
JKing <JKing@moderncampus.com>
2022-09-16 15:41:33 -0700

    Complete initial app set up

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.use("/", express.static("app/dist", {index: "index.html"}));
+app.use('/', express.static('app/dist', {index: 'index.html'}));
 
-app.set("port", (process.env.PORT || 8082));
+app.set('port', (process.env.PORT || 8082));
 
-app.listen(app.get("port"), () => {
-  console.log(`Server is listening on port ${app.get("port")}...`);
+app.listen(app.get('port'), () => {
+  console.log(`Server is listening on port ${app.get('port')}...`);
 });
