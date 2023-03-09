1474f8e4384c071ffc413bb4fbd315096bdf18ef
Joseph-Anthony-King <jking@moderncampus.com>
2023-03-08 23:05:54 -0800

    Add pinia and update npm packages

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import store from './store';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { loadFonts } from './plugins/webfontloader';
 
 loadFonts()
 
 createApp(App)
   .use(router)
-  .use(store)
   .use(vuetify)
+  .use(store)
+  .use(createPinia().use(piniaPluginPersistedstate))
   .mount('#app')
