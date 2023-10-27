9f685a004c121db75a1f41295c2c1e9296f50bb9
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2023-10-27 08:19:56 -0700

    Make the background image of the hero widget configurable

--- app/src/components/widgets/common/HeroWidget.vue
+++ app/src/components/widgets/common/HeroWidget.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="app-responsive-viewport">
     <v-card elevation="6" class="mx-16">
-      <v-row class="text-center home-banner">
+      <v-row class="text-center home-banner" :style="backgroundImage">
         <v-col cols="12">
           <h1 class="text-center centered-welcome-message text-padding">
             {{ headline }}
@@ -20,12 +20,24 @@
 </template>
 
 <script setup lang="ts">
+import { ComputedRef, computed } from 'vue';
+
 /* eslint-disable @typescript-eslint/no-unused-vars*/
 const props = defineProps({
   headline: {
     type: String,
     default: "",
   },
+  imageUrl: {
+    type: String,
+    default: "@/assets/banner.jpg",
+  }
+});
+
+const backgroundImage: ComputedRef<{ any :string }> = computed(() => {
+  return {
+    "background-image": props.headline
+  }
 });
 </script>
 
@@ -50,7 +62,6 @@ const props = defineProps({
   border: 0;
   border-color: transparent;
   min-height: 500px;
-  background-image: url("@/assets/banner.jpg");
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
